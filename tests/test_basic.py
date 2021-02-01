c0ac45540496c55fa0d3cb041eaf9d8dc451a353
Metin San <metinisan@gmail.com>
2021-02-01 21:24:49 +0100

    Improve reduce_chain algorithm

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,21 +12,18 @@ import numpy as np
 import astropy.units as u
 
 hp.disable_warnings()
-# data = '../../Cosmoglobe_test_data/reduced_chain_test.h5'
+data = '../../Cosmoglobe_test_data/reduced_chain_test.h5'
 # data = pathlib.Path('../../Cosmoglobe_test_data/chain_test.h5')
-data = '../../Cosmoglobe_test_data/chain_test.h5'
+# data = '../../Cosmoglobe_test_data/chain_test.h5'
 
-cosmo = Cosmoglobe(data, sample='mean', burnin=1)
-
+cosmo = Cosmoglobe(data, sample='mean')
+# cosmo.reduce_chainfile()
 cmb = cosmo.model('cmb', remove_monopole=True, remove_dipole=True)
 synch = cosmo.model('synch')
 dust = cosmo.model('dust')
 ff = cosmo.model('ff')
 ame = cosmo.model('ame')
 
-# frequency_map_30GHz = cosmo.full_sky(nu=30*u.GHz)
-# hp.mollview(frequency_map_30GHz[0])
-# plt.show()
 
 freqs, rms = cosmo.spectrum()
 for model, model_rms in rms.items():
@@ -37,3 +34,7 @@ plt.xlabel('Frequency [GHz]')
 plt.ylabel('RMS brightness temperature [muK]')
 plt.legend()
 plt.show()
+
+# frequency_map = cosmo.full_sky(nu=70*u.GHz, models=[ff, ame])
+# hp.mollview(synch[30*u.GHz][0], min=50, max=400, unit=synch.params['unit'], title=synch.params['type'])
+# plt.show()
