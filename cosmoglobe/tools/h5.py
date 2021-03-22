e8885538c457beeded4f50efa7350dee18ae468f
Metin San <metinisan@gmail.com>
2021-03-22 23:00:57 +0100

    Add more H5 routines

--- cosmoglobe/tools/h5.py
+++ cosmoglobe/tools/h5.py
@@ -5,8 +5,8 @@ import h5py
 import numpy as np
 
 
-param_group = 'parameters'
-
+param_group = 'parameters'  # Model parameter group name as implemented in commander
+_ignored_comps = ['md', 'radio', 'relquad'] # These will be dropped from component lists
 
 def _get_samples(file):
     """Returns a list of all samples present in a chain file"""
@@ -17,16 +17,35 @@ def _get_samples(file):
     return samples
 
 
-def _get_components(file):
+def _sample_to_int(samples, start=0):
+    """Converts a sample or a list of samples to integers"""
+    if isinstance(samples, (list, tuple)):
+        return [int(sample) + start for sample in samples]
+    
+    return int(samples) + start
+
+
+def _int_to_sample(samples, start=0):
+    """Converts an integer or multiple integers to sample string format"""
+    if isinstance(samples, (list, tuple)):
+        return [f'{sample + start:06d}' for sample in samples]
+    
+    return f'{samples + start:06d}'
+
+
+def _get_components(file, ignore_comps=True):
     """Returns a list of all components present in a chain file"""
     with h5py.File(file, 'r') as f:
         components = list(f[param_group].keys())
+
+    if ignore_comps:    
+        return [comp for comp in components if comp not in _ignored_comps]
 
     return components
 
 
 def _get_component_params(file, component):
-    """Returns a dictionary of the model parameters of one component"""
+    """Returns a dictionary of the model parameters of a component"""
     return_params = {}
     with h5py.File(file, 'r') as f:
         params = f[param_group][component]
@@ -40,7 +59,7 @@ def _get_component_params(file, component):
 
 
 def _get_items(file, sample, component, items):
-    """Return the value of one or many items in the chain file.
+    """Return the value of one or many items for a component in the chain file.
 
     Args:
     -----
@@ -71,7 +90,8 @@ def _get_items(file, sample, component, items):
 
 
 def _get_averaged_items(file, samples, component, items):
-    """Return the averaged value of one or many item in the chain file.
+    """Return the averaged value of one or many item for a component in the 
+    chain file.
 
     Args:
     -----
@@ -111,6 +131,22 @@ def _get_averaged_items(file, samples, component, items):
         return np.mean(item_to_return, axis=0)
 
 
+def _has_precomputed_map(file, component, item, sample=-1):
+    """Returns True if component contains precomputed map for item, else 
+    returns False.
+    """
+    if sample == -1:
+        sample = _get_samples(file)[-1]
+
+    with h5py.File(file, 'r') as f:
+        params = list(f[sample][component].keys())
+
+    if f'{item}_map' in params:
+        return True
+
+    return False
+
+
 
 if __name__ == '__main__':
     test_file = '/Users/metinsan/Documents/doktor/Cosmoglobe_test_data/bla.h5'
@@ -125,4 +161,5 @@ if __name__ == '__main__':
     #                    'ame', 
     #                    ('amp_alm', 'nu_p_map'))
     # print(items)
-    print(_get_component_params(test_file, 'dust'))
+    # print(_get_components(test_file, ignore_comps=False))
+    print(_has_precomputed_map(test_file, 'ff', 'Te'))
