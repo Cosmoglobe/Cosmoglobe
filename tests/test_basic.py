71a8f32422c6d0df8af054cd567e89d4dcd6a478
trygvels <tlsvalheim@gmail.com>
2021-03-26 11:16:24 +0100

    small changes

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,12 +2,11 @@
 For testing Cosmoglobe during development.
 
 """
-path = "/Users/svalheim/work/cosmoglobe-workdir"
+path = "/Users/svalheim/work/cosmoglobe-workdir/"
 from context import cosmoglobe
-
 import cosmoglobe.plot as cgp
 import healpy as hp
 
-map = hp.read_fits("cmb_c0001_k000200.fits")
+map = hp.read_map(path+"cmb_c0001_k000200.fits")
 
-cgp.mollplot()
+cgp.mollplot(map)
