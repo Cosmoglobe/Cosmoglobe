7f06c5ca6a8530e2b89d33ac3d2048c237aa7e52
Metin San <metinisan@gmail.com>
2021-04-02 00:25:39 +0200

    Merg devel with metin following large update

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,7 +7,7 @@ import healpy as hp
 import matplotlib.pyplot as plt
 import numpy as np
 from context import cosmoglobe
-
+import sys
 hp.disable_warnings()
 data = '../../Cosmoglobe_test_data/bla.h5'
 
@@ -15,27 +15,36 @@ data = '../../Cosmoglobe_test_data/bla.h5'
 import healpy as hp
 from cosmoglobe.sky.components import PowerLaw, ModifiedBlackBody
 from cosmoglobe.sky.model import Model
-from cosmoglobe.tools.map import IQUMap, to_IQU
-
-from cosmoglobe.tools.h5 import _get_items
+from cosmoglobe.tools.map import to_stokes, StokesMap
+import cosmoglobe.tools.h5 as h5
+import sys
 
 map_path = '/Users/metinsan/Documents/doktor/Cosmoglobe/cosmoglobe/data/BP7_70GHz_nocmb_n0256.fits'
 
 bandpass_data = '../../Cosmoglobe_test_data/wmap_bandpass.txt'
 nu_array, bandpass_array, _ = np.loadtxt(bandpass_data, unpack=True)
 
-nside = 32
-iqu = np.random.randint(low=1, high=5, size=(3, hp.nside2npix(nside)))
-# iqu = np.ones((3, hp.nside2npix(nside)))
+nside = 512
+# iqu = np.random.randint(low=1, high=5, size=(3, hp.nside2npix(nside)))
+intensity = np.array([np.ones(hp.nside2npix(nside)), np.zeros(hp.nside2npix(nside)), np.zeros(hp.nside2npix(nside))])
+pol = np.ones((3, hp.nside2npix(nside)))
+
 # i = np.random.randint(low=1, high=5, size=(hp.nside2npix(nside)))
+# i2 = np.ones(hp.nside2npix(nside))
 i = np.ones(hp.nside2npix(nside))
 scalar = 5
 
-synch = PowerLaw(amp=iqu, nu_ref=[30, 30, 30]*u.GHz, beta=iqu)
-dust = ModifiedBlackBody(amp=iqu*u.uK, nu_ref=[20,20,20]*u.GHz, beta=iqu, T=i*u.K)
-model = Model(components=[('synch', synch), ('dust', dust)])
+
+# synch = PowerLaw(comp_name='synch', amp=i*u.K, freq_ref=30*u.GHz, beta=i)
+# dust = ModifiedBlackBody(comp_name='dust', amp=pol*u.K, freq_ref=[20,10,10]*u.GHz, beta=pol, T=i*u.K)
 
 
-model.dust.get_emission(nu_array*u.GHz, bandpass_array*u.K)
 
-map_ = to_IQU(iqu, unit=u.m)
+# model = Model([synch, dust], nside=nside)
+
+model = h5.model_from_chain(file=data, sample=30)
+print(model)
+
+hp.mollview(model.get_emission(40*u.GHz).P, norm='hist')
+
+plt.show()
