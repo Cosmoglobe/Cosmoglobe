6cd6d2a03a0de54669f2106ad0757edae756ba10
trygvels <tlsvalheim@gmail.com>
2021-04-06 11:13:41 +0200

    Preparing merge with trygve

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,29 +13,37 @@ data = '../../Cosmoglobe_test_data/bla.h5'
 
 
 import healpy as hp
-from cosmoglobe.sky.components import PowerLaw, ModifiedBlackBody
-from cosmoglobe.sky.model import Model
-from cosmoglobe.tools.map import IQUMap, to_IQU
-
-from cosmoglobe.tools.h5 import _get_items
+import cosmoglobe.sky as sky
+from cosmoglobe.tools.map import to_stokes
+from cosmoglobe.tools.h5 import model_from_chain
 
 map_path = '/Users/metinsan/Documents/doktor/Cosmoglobe/cosmoglobe/data/BP7_70GHz_nocmb_n0256.fits'
 
 bandpass_data = '../../Cosmoglobe_test_data/wmap_bandpass.txt'
 nu_array, bandpass_array, _ = np.loadtxt(bandpass_data, unpack=True)
 
-nside = 32
+nside = 64
 iqu = np.random.randint(low=1, high=5, size=(3, hp.nside2npix(nside)))
 # iqu = np.ones((3, hp.nside2npix(nside)))
-# i = np.random.randint(low=1, high=5, size=(hp.nside2npix(nside)))
-i = np.ones(hp.nside2npix(nside))
+i = np.random.randint(low=1, high=5, size=(hp.nside2npix(nside)))
+# i = np.ones(hp.nside2npix(nside))
 scalar = 5
 
-synch = PowerLaw(amp=iqu, nu_ref=[30, 30, 30]*u.GHz, beta=iqu)
-dust = ModifiedBlackBody(amp=iqu*u.uK, nu_ref=[20,20,20]*u.GHz, beta=iqu, T=i*u.K)
-model = Model(components=[('synch', synch), ('dust', dust)])
 
+# dust = sky.BlackBodyCMB('dust', amp=i*u.uK)
+# print(dust.amp)
+# model = model_from_chain(data, nside=16, burn_in=20)
+# print(model)
+# emission = model.get_emission([10, 500]*u.GHz)
+# hp.mollview(emission[0].I, norm='hist')
+# hp.mollview(emission[1].I, norm='hist')
 
-model.dust.get_emission(nu_array*u.GHz, bandpass_array*u.K)
+i_30 = hp.smoothing(i, (30*u.arcmin).to(u.rad).value, use_pixel_weights=True)
+i_60 = hp.smoothing(i_30, (np.sqrt(60**2 - 30**2)*u.arcmin).to(u.rad).value, use_pixel_weights=True)
+i__60 = hp.smoothing(i, (60*u.arcmin).to(u.rad).value, use_pixel_weights=True)
 
-map_ = to_IQU(iqu, unit=u.m)
+print(i_60 - i__60)
+hp.mollview(i_60)
+hp.mollview(i__60)
+hp.mollview(i_60-i__60)
+plt.show()
