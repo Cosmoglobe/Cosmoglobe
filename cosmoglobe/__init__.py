d918ce5d30e81e17b9e9aaa5960f6ef3bd023bac
Metin San <metinisan@gmail.com>
2021-10-11 15:27:13 +0200

    Added compsep chainfile

--- cosmoglobe/__init__.py
+++ cosmoglobe/__init__.py
@@ -1,3 +1,4 @@
 from cosmoglobe.h5.model import model_from_chain
+from cosmoglobe.hub import get_test_chain
 from cosmoglobe.plot import plot, gnom, trace
 from cosmoglobe.h5.chain import Chain
--- cosmoglobe/hub.py
+++ cosmoglobe/hub.py
@@ -1,12 +1,13 @@
-import pathlib
 from enum import Enum
 from typing import Optional
 
 from astropy.utils.data import download_file
 
+from cosmoglobe.h5.chain import Chain
+
 # from cosmoglobe.chain.h5 import model_from_h5
 
-data_url = pathlib.Path("http://cosmoglobe.uio.no/releases/")
+DATA_URL = "http://cosmoglobe.uio.no/BeyondPlanck/compsep/"
 
 
 class Release(Enum):
@@ -15,40 +16,64 @@ class Release(Enum):
     BP10 = LATEST = "BP10"
 
 
-def sky_model(nside: int, release: Optional[str] = None, cache: bool = True) -> "Model":
-    """Initialize the Cosmoglobe Sky Model from a official release.
+def get_test_chain(cache: bool = True) -> Chain:
+    """Returns a minimal Cosmoglobe chain.
 
-    By default, the model is initialized from the latest stable cosmoglobe data
-    release. The data required to initialize the model is downloaded and cached
-    by default every time a unique nside is selected.
-
-    For a list of available releases see the get_releases function.
-
+    This function will download the chain if it is not already cached.
+    The chain is of size 1.6 GB.
+    
     Parameters
     ----------
-    nside : int
-        Healpix resolution parameter to initialize the model at.
-    release : int, str
-        Data release version. By default, the latest stable release is selected,
-        which corresponds to -1.
-        Default: -1
-    cache : bool
-        Wether to cache the data after the download. Default: True.
+    cache
+        Boolean for wether or not to cache the downloaded chain. Defaults to
+        True
+    
+    Returns
+    -------
+    Chain
+        Initialized Chainfile object.
     """
 
-    filename = f"model_{nside}.h5"
-    path = "/Users/metinsan/Documents/doktor/models/test1/"
+    filename = DATA_URL + "compsep_chain.h5"
+    file = download_file(filename, cache=cache)
 
-    if release is None:
-        release = Release.LATEST
-    else:
-        try:
-            release = Release[release.upper()]
-        except KeyError:
-            raise ValueError(
-                f"Release {release} does not exist. Available releases are: "
-                f"{[name for name, _ in Release.__members__.items() if name != 'LATEST']}"
-            )
+    return Chain(file)
+
+
+# def sky_model(nside: int, release: Optional[str] = None, cache: bool = True) -> "Model":
+#     """Initialize the Cosmoglobe Sky Model from a official release.
+
+#     By default, the model is initialized from the latest stable cosmoglobe data
+#     release. The data required to initialize the model is downloaded and cached
+#     by default every time a unique nside is selected.
+
+#     For a list of available releases see the get_releases function.
+
+#     Parameters
+#     ----------
+#     nside : int
+#         Healpix resolution parameter to initialize the model at.
+#     release : int, str
+#         Data release version. By default, the latest stable release is selected,
+#         which corresponds to -1.
+#         Default: -1
+#     cache : bool
+#         Wether to cache the data after the download. Default: True.
+#     """
+
+#     filename = f"model_{nside}.h5"
+#     path = "/Users/metinsan/Documents/doktor/models/test1/"
+
+#     if release is None:
+#         release = Release.LATEST
+#     else:
+#         try:
+#             release = Release[release.upper()]
+#         except KeyError:
+#             raise ValueError(
+#                 f"Release {release} does not exist. Available releases are: "
+#                 f"{[name for name, _ in Release.__members__.items() if name != 'LATEST']}"
+#             )
 
 
 
