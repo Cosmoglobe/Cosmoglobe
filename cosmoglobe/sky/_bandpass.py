Fixed bug in bandpass integration for K_CMB output units.

cosmoglobe/sky/_bandpass.py
@@ -1,14 +1,25 @@
-from typing import Callable, Dict, Union
+from typing import Union
 
-from astropy.units import Quantity, Unit, UnitTypeError
+from astropy.units import Quantity, Unit
 import numpy as np
 
-from cosmoglobe.utils.utils import str_to_astropy_unit
-from cosmoglobe.sky._intensity_derivatives import INTENSITY_DERIVATIVE_MAPPINGS
+from cosmoglobe.sky._intensity_derivative import get_intensity_derivative
 
 
 def get_normalized_bandpass(freqs: Quantity, bandpass: Quantity) -> Quantity:
-    """Normalizes a bandpass to units of unity under integration."""
+    """Normalizes a bandpass to units of unity under integration.
+
+    Parameters
+    ----------
+    freqs
+        Frequencies corresponding to bandpass weights.
+    bandpass
+        The bandpass profile.
+
+    Returns
+    -------
+        Bandpass with unit integral over np.trapz.
+    """
 
     return bandpass / np.trapz(bandpass, freqs)
 
@@ -19,30 +30,40 @@ def get_bandpass_coefficient(
     input_unit: Union[str, Unit],
     output_unit: Union[str, Unit],
 ) -> Quantity:
-    """Returns the bandpass coefficient."""
+    """Returns the bandpass coefficient.
+
+    Parameters
+    ----------
+    freqs
+        Frequencies corresponding to bandpass weights.
+    bandpass
+        The bandpass profile.
+    input_unit
+        The unit of the cosmoglobe data.
+    ouput_unit
+        The requested output unit of the simulated emission
+
+    Returns
+    -------
+    coefficient
+        Bandpass coefficient representing the unitconversion between the
+        input/ouput unit over the bandpass.
+    """
 
     if isinstance(input_unit, str):
-        input_unit = str_to_astropy_unit(input_unit)
+        in_unit = input_unit
+    else:
+        in_unit = input_unit.to_string().replace(" ", "")
     if isinstance(output_unit, str):
-        output_unit = str_to_astropy_unit(output_unit)
+        out_unit = output_unit
+    else:
+        out_unit = output_unit.to_string().replace(" ", "")
 
-    units = {"in": input_unit, "out": output_unit}
+    in_intensity_derivative = get_intensity_derivative(in_unit)
+    out_intensity_derivative = get_intensity_derivative(out_unit)
 
-    intensity_derivatives: Dict[str, Callable[[Quantity], Quantity]] = {}
-
-    for key, unit in units.items():
-        for intensity_deriv in INTENSITY_DERIVATIVE_MAPPINGS:
-            intensity_unit = str_to_astropy_unit(intensity_deriv)
-            if intensity_unit._is_equivalent(unit):
-                intensity_derivatives[key] = INTENSITY_DERIVATIVE_MAPPINGS[
-                    intensity_deriv
-                ]
-                break
-        else:
-            raise UnitTypeError("unrecognized unit for intensity derivatives")
-
-    coefficient = np.trapz(
-        bandpass * intensity_derivatives["in"](freqs), freqs
-    ) / np.trapz(bandpass * intensity_derivatives["out"](freqs), freqs)
+    coefficient = np.trapz(bandpass * in_intensity_derivative(freqs), freqs) / np.trapz(
+        bandpass * out_intensity_derivative(freqs), freqs
+    )
 
     return coefficient

cosmoglobe/sky/_beam.py
@@ -5,11 +5,9 @@ from astropy.units import Quantity, Unit, quantity_input
 import numpy as np
 import healpy as hp
 
-from cosmoglobe.utils.utils import gaussian_beam_2D
 from cosmoglobe.sky._constants import DEFAULT_BEAM_FWHM
 
 TEST_BEAM_BL = "/Users/metinsan/Documents/doktor/Cosmoglobe_test_data/wmap_beam.txt"
-
 
 
 @quantity_input(fwhm=Unit("rad"))
@@ -29,6 +27,24 @@ def get_sigma(fwhm: Quantity) -> Quantity:
     return fwhm / (2 * sqrt(2 * log(2)))
 
 
+def gaussian_beam_2D(r: np.ndarray, sigma: float) -> np.ndarray:
+    """Returns the Gaussian beam in 2D in polar coordinates.
+
+    Parameters
+    ----------
+    r
+        Angular distance.
+    sigma
+        The sigma of the Gaussian (beam radius).
+
+    Returns
+    -------
+        Gaussian beam.
+    """
+
+    return r * np.exp(-(r ** 2) / (2 * sigma ** 2)) / (sigma * np.sqrt(2 * np.pi))
+
+
 def pointsources_to_healpix(
     point_sources: Quantity,
     catalog: np.ndarray,
@@ -43,7 +59,7 @@ def pointsources_to_healpix(
 
     N_FWHM = 2  # FWHM cutoff for the truncated beam
 
-    healpix = Quantity(
+    healpix_map = Quantity(
         np.zeros((point_sources.shape[0], hp.nside2npix(nside))),
         unit=point_sources.unit,
     )
@@ -56,14 +72,14 @@ def pointsources_to_healpix(
             "fwhm not specified. Mapping point sources to pixels "
             "without beam smoothing"
         )
-        point_source_pixels = hp.ang2pix(nside, *catalog, lonlat=True)
+        pixels = hp.ang2pix(nside, *catalog, lonlat=True)
         for IQU, emission in enumerate(point_sources):
-            healpix[IQU, point_source_pixels] = emission
+            healpix_map[IQU, pixels] = emission
 
         pixel_area = hp.nside2pixarea(nside) * Unit("sr")
-        return healpix / pixel_area
+        return healpix_map / pixel_area
 
-    # Applying a truncated beam to each point source
+    # Applying a truncated Gaussian beam to each point source
     else:
         sigma = get_sigma(fwhm)
         pixel_resolution = hp.nside2resol(nside)
@@ -87,10 +103,10 @@ def pointsources_to_healpix(
             )
             beam = gaussian_beam_2D(angular_distance, sigma.value)
             for IQU, emission in enumerate(point_sources):
-                healpix[IQU, pixels] += emission[idx] * beam
+                healpix_map[IQU, pixels] += emission[idx] * beam
 
         beam_area = 2 * pi * sigma ** 2
-        return healpix / beam_area
+        return healpix_map / beam_area
 
 
 if __name__ == "__main__":
@@ -98,5 +114,5 @@ if __name__ == "__main__":
 
     b_l = np.loadtxt(TEST_BEAM_BL, skiprows=10)
     beam = hp.bl2beam(b_l[:, 1], b_l[:, 0])
-    plt.plot(b_l[:, 0 ], beam)
+    plt.plot(b_l[:, 0], beam)
     plt.show()