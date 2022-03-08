89ac1a4c69467a1f41e5a976d52dc204d37dc0c3
trygvels <tlsvalheim@gmail.com>
2022-03-08 16:41:51 +0100

    Added click functionality

--- cosmoglobe/__main__.py
+++ cosmoglobe/__main__.py
@@ -1,5 +1,6 @@
 import click
 from cosmoglobe.plot.click_plotting import commands_plotting
+from cosmoglobe.h5.click_h5 import commands_h5
 
 CONTEXT_SETTINGS = dict(max_content_width=100)
 
@@ -7,7 +8,7 @@ CONTEXT_SETTINGS = dict(max_content_width=100)
 def cli():
     pass
 
-cli = click.CommandCollection(sources=[commands_plotting,], context_settings=CONTEXT_SETTINGS)
+cli = click.CommandCollection(sources=[commands_plotting, commands_h5], context_settings=CONTEXT_SETTINGS)
 
 if __name__ == '__main__':
     cli()
