ccf016a2de81f71ac9dd97e3381693027cfd7fa8
Eirik Gjerløw <egjerlow@protonmail.com>
2022-06-28 14:59:11 +0200

    Delete unused files

--- _exceptions.py
+++
@@ -1,7 +0,0 @@
-
-class OutputDirectoryException(Exception):
-    """Raised if there is an issue related to the output directory."""
-
-    def __init__(self, message: str) -> None:
-        self.message = message
-        super().__init__(self.message)
