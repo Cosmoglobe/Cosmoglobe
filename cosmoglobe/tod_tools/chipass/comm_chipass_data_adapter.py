a1b8c906bf48a787cd9af69a6ff2a0f7e7d969b6
Danielle Rae Sponseller <daniersp@owl34.uio.no>
2025-04-04 13:52:09 +0200

    Updated CHIPASS data adapter

--- cosmoglobe/tod_tools/chipass/comm_chipass_data_adapter.py
+++ cosmoglobe/tod_tools/chipass/comm_chipass_data_adapter.py
@@ -1,104 +1,35 @@
 import numpy as np
 from cosmoglobe.tod_tools import CommanderDataAdapter
 from dataclasses import dataclass
-from pathlib import Path
 from astropy.time import Time
-from astropy.coordinates import SkyCoord, FK4
-from astropy.coordinates import HeliocentricMeanEcliptic, get_body, solar_system_ephemeris
+import astropy.coordinates as coord
+from astropy.coordinates import SkyCoord, HeliocentricMeanEcliptic, get_body, AltAz
 from astropy import units as u
+from astropy.io import fits
 import healpy as hp
-import os
-
-GC = SkyCoord(l=0*u.degree, b=0*u.degree, frame='galactic')
+import glob
 
 
 @dataclass
-class CommIRASDataAdapter(CommanderDataAdapter):
+class CommCHIPASSDataAdapter(CommanderDataAdapter):
 
     def __post_init__(self):
-        self.iras_data_path = Path("/mn/stornext/d5/data/duncanwa/IRAS/sopobs_data")
         self.currchunk = None
-        self.bands = ['12', '25', '60', '100']
-        self.nside = 2048
-        self.t0 = Time('1981-01-01', scale='utc')
+        self.bands = ['1395']
+        self.nside = 1024
         self.dets = {
-            '12': [f'{el:02}' for el in np.concatenate((np.arange(23, 31), np.arange(48, 54)))],
-            '25': [f'{el:02}' for el in np.concatenate((np.array([16,18,19,21, 22]), np.arange(40,46)))],
-            '60': [f'{el:02}' for el in np.concatenate((np.array([8,9,10,13,14,15]), np.array([32,33,34,35,37])))],
-            '100': [f'{el:02}' for el in np.concatenate((np.arange(1, 8), np.arange(56, 62)))]
+            '1395': [f'{el:02}' for el in np.arange(1, 14)]
         }
-        self.fsamp ={'12':16, '25':16, '60':8, '100':4}
-        self.npsi = 8  # For IRAS this shouldn't matter
-        self.Omegas = {1: 14.5,
-                       2: 12.7,
-                       3: 13.0,
-                       4: 11.53,
-                       5: 12.0,
-                       6: 12.4,
-                       7:12.6,
-                       8:7.2,
-                       9:6.7,
-                       10:6.6,
-                       11:2.8,
-                       12:4.3,
-                       13:6.6,
-                       14:6.1,
-                       15:6.2,
-                       16:3.5,
-                       18:3.6,
-                       19:2.8,
-                       21:2.8,
-                       22:3.1,
-                       23:2.9,
-                       24:3.0,
-                       25:3.2,
-                       26:1.2,
-                       27:2.0,
-                       28:3.1,
-                       29:2.5,
-                       30:2.8,
-                       55:7.1,
-                       56:14.0,
-                       57:13.2,
-                       58:11.2,
-                       59:11.7,
-                       60:13.3,
-                       61:13.5,
-                       62:10.6,
-                       31:2.1,
-                       32:6.4,
-                       33:5.9,
-                       34:6.5,
-                       35:6.3,
-                       37:6.6,
-                       38:3.9,
-                       39:1.4,
-                       40:3.1,
-                       41:3.1,
-                       42:3.4,
-                       43:3.2,
-                       44:3.2,
-                       45:3.2,
-                       46:2.4,
-                       47:0.77,
-                       48:3.1,
-                       49:2.9,
-                       50:3.0,
-                       51:2.7,
-                       52:2.5,
-                       53:2.8,
-                       54:2.0}
-
-        self.num_chunks = 5787
-        self.num_segments = self.num_chunks // 10
-
+        self.fsamp = {'1395':8}
+        self.npsi = 1
+        self.num_chunks = 50690
+        self.num_segments = self.num_chunks // 100 + 1
 
     def get_num_segments(self, band):
-#        return 3
         return self.num_segments
 
     def get_experiment_name(self):
-        return 'iras'
+        return 'chipass'
 
     def get_npsi(self):
         return self.npsi
@@ -121,25 +52,22 @@ class CommIRASDataAdapter(CommanderDataAdapter):
     def get_chunk_indices(self, band, segment):
         out_indices = []
         if segment != self.num_segments:
-            iterator = range((segment-1)*10+1, segment*10+1)
+            iterator = range((segment-1)*100+1, segment*100+1)
         else:
-            iterator = range(segment*10+1, self.num_chunks+1)
+            iterator = range((segment-1)*100+1, self.num_chunks+1)
         for idx in iterator:
             to_be_added = True
+            '''
             for det in self.dets[band]:
-                if idx == 273:
-                    to_be_added = False
-                    break
-                f = f'{self.iras_data_path}/det_{det}/sopobs_{idx:04}.npy'
                 if not os.path.exists(f):
                     to_be_added = False
                     break
+            '''
             if to_be_added:
                 out_indices.append(idx)
         return out_indices
 
     def get_bands(self):
-#        return self.bands[:2]
         return self.bands
 
     def set_chunk_index(self, chunk_idx: int):
@@ -147,19 +75,67 @@ class CommIRASDataAdapter(CommanderDataAdapter):
         self.chunk_starttime = None
 
     def get_chunk_data(self, band, detector):
-
-        f = f'{self.iras_data_path}/det_{detector}/sopobs_{self.currchunk:04}.npy'
-        if os.path.exists(f):
-            t, lon, lat, tod = np.load(f)
-        else:
-            # Should not really happen now, since we are doing this filtering earlier
-            return [None] * 4
+        data_dir = '/mn/stornext/d16/cmbco/ola/chipass/l1_data/'
+        file_list = sorted(glob.glob(data_dir + "/**/*.sdfits", recursive=True))
+        f = file_list[self.currchunk-1]
+        data_raw = fits.open(f)
+        # time
+        date = np.array(data_raw[1].data['date-obs']).reshape(-1,13)[:, 0]
+        t0 = Time(date, format='isot', scale='utc').mjd
+        tt = data_raw[1].data['time'].reshape(-1,13)[:, 0]  # time, in seconds, since start of day (UTC)
+        nt = len(tt)  # number of samples in time
+        t = t0 + tt / 3600.0 / 24
+        # Pointing info 
+        az = data_raw[1].data['AZIMUTH'].reshape(-1,13)
+        el = data_raw[1].data['ELEVATIO'].reshape(-1,13)
+        # Lat/Lon/h (WGS84) of Parkes telescope 32.9984064, 148.2635101, 414.80
+        # (https://www.narrabri.atnf.csiro.au/observing/users_guide/html/chunked/apg.html)
+        loc = coord.EarthLocation(lon=148.2635101 * u.deg, lat=-32.9984064 * u.deg, height=414.80 * u.m)
+        time = Time(t, format='mjd')
+        AltAz = SkyCoord(alt=el*u.deg, az=az*u.deg, obstime=time[:, None], frame='altaz', location=loc)
+        point = np.array([AltAz.galactic.l.degree, AltAz.galactic.b.degree, np.zeros_like(AltAz.galactic.b.degree)]).transpose((1, 2, 0))
+        point = point.astype(np.float32)
+        lon = np.copy(point[:, int(detector)-1, 0])
+        lat = np.copy(point[:, int(detector)-1, 1])
+        pix = hp.ang2pix(self.nside, lon, lat, lonlat=True)
+        # tod
+        tod = data_raw[1].data['Data'][:,0,0,:,:]
+        tod = np.reshape(tod, (-1, 13, 2, 1024))
+        tsys = data_raw[1].data['Tsys']
+        tsys = np.reshape(tsys, (-1, 13, 2))
+        flag = data_raw[1].data['flagged'][:,0,0,:,:]
+        flag = np.reshape(flag, (-1, 13, 2, 1024))
+        mask = np.array(1.0 - flag).astype(float)
+        ratio = tod[:, :, :, :] / tsys[:, :, :, None]
+        n_med = 25
+        n_half = n_med // 2
+        n = ratio.shape[0]
+        bst = np.zeros_like(ratio)
+        for i in range(n):
+            low = max(i-n_half, 0)
+            hi = min(i+n_half, n)
+            bst[i] = np.nanmedian(ratio[low:hi], axis=0)
+        n = tsys.shape[0]
+        bt = np.zeros_like(tsys)
+        for i in range(n):
+            low = max(i-n_half, 0)
+            hi = min(i+n_half, n)
+            bt[i] = np.nanmedian(tsys[low:hi], axis=0)
+        s_prime = tod / bst - bt[:, :, :, None]  # Calibrated data in Jy
+        # bandpass integration (using median for now)
+        s_cont = np.zeros((nt, 13, 2, 4))
+        mask[:, :, :, 85:125] = 0  # remove 
+        mask[(mask == 0)] = np.nan
+        for i in range(4):
+            s_cont[:, :, :, i] = np.nanmedian(mask[:, :, :, i*256:(i+1)*256] * s_prime[:, :, :, i*256:(i+1)*256], axis=3)
+        # average over four bandpasses
+        tod_new = np.sqrt(s_cont[:, :, 0] ** 2 + s_cont[:, :, 1] ** 2).mean(2)
+        # get data for this detector
+        tod = np.copy(tod_new[:, int(detector)-1])
 
         if self.chunk_starttime is None:
-            self.chunk_starttime = t[0]*u.s + self.t0
-            self.chunk_endtime = t[-1]*u.s + self.t0
-
-        tod /= self.Omegas[int(detector)] * 1e-7
+            self.chunk_starttime = Time(date[0], format='isot', scale='utc') + tt[0]*u.s
+            self.chunk_endtime = Time(date[0], format='isot', scale='utc') + tt[-1]*u.s
 
         flag_tot = np.zeros(len(t))
         flag_tot[~np.isfinite(lon)] += 2**0
@@ -171,14 +147,6 @@ class CommIRASDataAdapter(CommanderDataAdapter):
 
         psi = np.zeros_like(tod)
         
-        good_data = flag_tot == 0
-        sc = SkyCoord(ra=lon[good_data], dec=lat[good_data], unit='deg',
-                equinox='B1950.0', obstime='J1983.5', frame=FK4)
-        coords = sc.transform_to(GC)
-        lon[good_data] = coords.l.value
-        lat[good_data] = coords.b.value
-        pix = hp.ang2pix(self.nside, lon, lat, lonlat=True) 
-
         return tod, pix, psi, flag_tot
 
 
