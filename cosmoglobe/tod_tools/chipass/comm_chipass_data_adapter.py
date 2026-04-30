0915969fdb26fbc326ed2523d23e8bbaa0d2ed0b
Danielle Sponseller <danielle.sponseller@chalmers.se>
2026-04-30 15:59:57 +0200

    Use raw uncalibrated data, and corrected pointing

--- cosmoglobe/tod_tools/chipass/comm_chipass_data_adapter.py
+++ cosmoglobe/tod_tools/chipass/comm_chipass_data_adapter.py
@@ -3,7 +3,7 @@ from cosmoglobe.tod_tools import CommanderDataAdapter
 from dataclasses import dataclass
 from astropy.time import Time
 import astropy.coordinates as coord
-from astropy.coordinates import SkyCoord, HeliocentricMeanEcliptic, get_body, AltAz
+from astropy.coordinates import SkyCoord, FK5, HeliocentricMeanEcliptic, get_body
 from astropy import units as u
 from astropy.io import fits
 import healpy as hp
@@ -83,55 +83,74 @@ class CommCHIPASSDataAdapter(CommanderDataAdapter):
         date = np.array(data_raw[1].data['date-obs']).reshape(-1,13)[:, 0]
         t0 = Time(date, format='isot', scale='utc').mjd
         tt = data_raw[1].data['time'].reshape(-1,13)[:, 0]  # time, in seconds, since start of day (UTC)
-        nt = len(tt)  # number of samples in time
         t = t0 + tt / 3600.0 / 24
+        nt = len(tt)  # number of integrations in scan
         # Pointing info 
-        az = data_raw[1].data['AZIMUTH'].reshape(-1,13)
-        el = data_raw[1].data['ELEVATIO'].reshape(-1,13)
-        # Lat/Lon/h (WGS84) of Parkes telescope 32.9984064, 148.2635101, 414.80
-        # (https://www.narrabri.atnf.csiro.au/observing/users_guide/html/chunked/apg.html)
-        loc = coord.EarthLocation(lon=148.2635101 * u.deg, lat=-32.9984064 * u.deg, height=414.80 * u.m)
-        time = Time(t, format='mjd')
-        AltAz = SkyCoord(alt=el*u.deg, az=az*u.deg, obstime=time[:, None], frame='altaz', location=loc)
-        point = np.array([AltAz.galactic.l.degree, AltAz.galactic.b.degree, np.zeros_like(AltAz.galactic.b.degree)]).transpose((1, 2, 0))
-        point = point.astype(np.float32)
-        lon = np.copy(point[:, int(detector)-1, 0])
-        lat = np.copy(point[:, int(detector)-1, 1])
+        ra = data_raw[1].data['CRVAL3'].reshape(nt, 13)
+        dec = data_raw[1].data['CRVAL4'].reshape(nt, 13)
+        sc = SkyCoord(ra=ra[:, int(detector)-1], dec=dec[:, int(detector)-1], unit='deg', frame=FK5, equinox='J2000.0')
+        gc = sc.transform_to('galactic')
+        lon = gc.l.degree
+        lat = gc.b.degree
         pix = hp.ang2pix(self.nside, lon, lat, lonlat=True)
         # tod
         tod = data_raw[1].data['Data'][:,0,0,:,:]
-        tod = np.reshape(tod, (-1, 13, 2, 1024))
-        tsys = data_raw[1].data['Tsys']
-        tsys = np.reshape(tsys, (-1, 13, 2))
+        tod = np.reshape(tod, (nt, 13, 2, 1024))
         flag = data_raw[1].data['flagged'][:,0,0,:,:]
-        flag = np.reshape(flag, (-1, 13, 2, 1024))
+        flag = np.reshape(flag, (nt, 13, 2, 1024))
         mask = np.array(1.0 - flag).astype(float)
+        # calibrate new
+        '''
+        tsys = data_raw[1].data['Tsys']
+        tsys = np.reshape(tsys, (nt, 13, 2))
+        tod[np.where(mask == 0.0)] = np.nan # added: apply mask before calculating running median filter
         ratio = tod[:, :, :, :] / tsys[:, :, :, None]
-        n_med = 25
+        n_med = 10
         n_half = n_med // 2
         n = ratio.shape[0]
-        bst = np.zeros_like(ratio)
+        run_med = np.zeros_like(ratio)
         for i in range(n):
             low = max(i-n_half, 0)
-            hi = min(i+n_half, n)
-            bst[i] = np.nanmedian(ratio[low:hi], axis=0)
+            hi = min(i+n_half+1, n)
+            run_med[i] = np.nanmedian(ratio[low:hi], axis=0)
+        bst = np.nanmin(run_med, axis=0)
         n = tsys.shape[0]
-        bt = np.zeros_like(tsys)
+        run_med = np.zeros_like(tsys)
         for i in range(n):
             low = max(i-n_half, 0)
-            hi = min(i+n_half, n)
-            bt[i] = np.nanmedian(tsys[low:hi], axis=0)
-        s_prime = tod / bst - bt[:, :, :, None]  # Calibrated data in Jy
-        # bandpass integration (using median for now)
-        s_cont = np.zeros((nt, 13, 2, 4))
+            hi = min(i+n_half+1, n)
+            run_med[i] = np.nanmedian(tsys[low:hi], axis=0)
+        bt = np.nanmin(run_med, axis=0)
+        s_prime = tod / bst[None, :, :, :] - bt[None, :, :, None]  # Calibrated data in Jy
+        '''
+        s_prime = np.copy(tod) # do not calibrate, use raw TOD
+        # Remove 21 cm channels
         mask[:, :, :, 85:125] = 0  # remove 
+        # remove additional bands
+        mask[:, :, :, 0:10] = 0 # 2:10
+        mask[:, :, :, 151:159] = 0
+        mask[:, :, :, 236:244] = 0
+        mask[:, :, :, 294:298] = 0
+        mask[:, :, :, 390:398] = 0
+        mask[:, :, :, 423:427] = 0
+        mask[:, :, :, 483:491] = 0
+        mask[:, :, :, 727:735] = 0
+        mask[:, :, :, 972:980] = 0
         mask[(mask == 0)] = np.nan
+        # bandpass integration (using median for now)
+        # divide channels into four bands and get the median in each
+        s_cont = np.zeros((nt, 13, 2, 4))
         for i in range(4):
             s_cont[:, :, :, i] = np.nanmedian(mask[:, :, :, i*256:(i+1)*256] * s_prime[:, :, :, i*256:(i+1)*256], axis=3)
-        # average over four bandpasses
+        # combine the polarizations and average over the four bandpasses
         tod_new = np.sqrt(s_cont[:, :, 0] ** 2 + s_cont[:, :, 1] ** 2).mean(2)
         # get data for this detector
         tod = np.copy(tod_new[:, int(detector)-1])
+        # convert units from Jy beam^-1 to MJy sr^-1 based on beam size (FWHP approx 14.3 arcmin for all beams)
+        #tod *= 0.057793
+        # convert units from Jy beam^-1 to mK and apply offset following Calabretta2014 -- 0.44 K/(Jy beam^-1), + 3.3 K offset
+        #tod *= 440.0 # mK
+        #tod += 3300.0
 
         if self.chunk_starttime is None:
             self.chunk_starttime = Time(date[0], format='isot', scale='utc') + tt[0]*u.s
@@ -140,6 +159,7 @@ class CommCHIPASSDataAdapter(CommanderDataAdapter):
         flag_tot = np.zeros(len(t))
         flag_tot[~np.isfinite(lon)] += 2**0
         flag_tot[~np.isfinite(tod)] += 2**1
+        #if np.all(flag_tot): print('fully-masked scan', f)
 
         tod[flag_tot != 0] = 0
         lon[flag_tot != 0] = 0
